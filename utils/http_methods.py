17603a9a5500db050ca041007f991b4f9020cf12
Denis Antonov <den69ant@yandex.ru>
2022-09-06 16:37:07 +0300

    logging implementations

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,5 +1,7 @@
 import requests
 
+from utils.logger import Logger
+
 """Список HTTP методов"""
 
 class HttpMethods:
@@ -8,22 +10,30 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
 
